17807de7188fc500bac60ff9e3fcdcf1feaaf389
yusu Jang <dbtnss@naver.com>
2021-06-11 11:37:55 +0900

    set LoginForm, UserProfile

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -7,20 +7,29 @@ const ButtonWrapper = styled.div`
   margin-top: 10px;
 `
 
-const LoginForm = () => {
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`
+
+const LoginForm = ({ setIsLoggedIn }) => {
   const [id, setId] = useState('')
   const [password, setPassword] = useState('')
 
-  const onChangeId = useCallback((e) => {
+  const onChangeId = useCallback(e => {
     setId(e.target.value)
   }, [])
 
-  const onChangePassword = useCallback((e) => {
+  const onChangePassword = useCallback(e => {
     setPassword(e.target.value)
   }, [])
 
+  const onSubmitForm = useCallback(() => {
+    console.log(id, password)
+    setIsLoggedIn(true)
+  }, [id, password])
+
   return (
-    <Form>
+    <FormWrapper onFinish={onSubmitForm}>
       <div>
         <label htmlFor="user-id">아이디</label>
         <br />
@@ -41,13 +50,13 @@ const LoginForm = () => {
         <Button type="primary" htmlType="submit" loading={false}>
           로그인
         </Button>
-        <Link href="/singup">
+        <Link href="/signup">
           <a>
             <Button>회원가입</Button>
           </a>
         </Link>
       </ButtonWrapper>
-    </Form>
+    </FormWrapper>
   )
 }
 
