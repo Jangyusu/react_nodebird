bb6a923b68697a5625ea66bcdb6aea116a70c6ff
yusu Jang <dbtnss@naver.com>
2021-06-13 21:33:19 +0900

    update imagesZoom conponent

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 import { Menu, Input, Row, Col } from 'antd'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import { useSelector } from 'react-redux'
 
 import UserProfile from '../components/UserProfile'
@@ -11,11 +11,27 @@ const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `
 
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+
+  .ant-col:first-child {
+    padding-left: 0 !important;
+  }
+
+  .ant-col:last-child {
+    padding-right: 0 !important;
+  }
+`
+
 const AppLayout = ({ children }) => {
   const { isLoggedIn } = useSelector(state => state.user)
 
   return (
     <div>
+      <Global />
       <Menu mode="horizontal">
         <Menu.Item key="index">
           <Link href="/">
