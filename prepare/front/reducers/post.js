d4e7226d27c4260b263f993af2531082aeb4bc0c
yusu Jang <dbtnss@naver.com>
2021-06-18 23:44:00 +0900

    use immer for reducers

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,4 +1,5 @@
 import shortId from 'shortid'
+import produce from 'immer'
 
 export const initialState = {
   mainPosts: [
@@ -103,79 +104,58 @@ export const removePost = data => ({
 })
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST_REQUEST:
-      return {
-        ...state,
-        addPostLoading: true,
-        addPostDone: false,
-        addPostError: null,
-      }
-    case ADD_POST_SUCCESS:
-      return {
-        ...state,
-        mainPosts: [dummyPost(action.data), ...state.mainPosts],
-        addPostLoading: false,
-        addPostDone: true,
-      }
-    case ADD_POST_FAILURE:
-      return {
-        ...state,
-        addPostLoading: false,
-        addPostError: action.error,
-      }
-    case REMOVE_POST_REQUEST:
-      return {
-        ...state,
-        removePostLoading: true,
-        removePostDone: false,
-        removePostError: null,
-      }
-    case REMOVE_POST_SUCCESS:
-      return {
-        ...state,
-        mainPosts: state.mainPosts.filter(v => v.id !== action.data),
-        removePostLoading: false,
-        removePostDone: true,
-      }
-    case REMOVE_POST_FAILURE:
-      return {
-        ...state,
-        removePostLoading: false,
-        removePostError: action.error,
-      }
-    case ADD_COMMENT_REQUEST:
-      return {
-        ...state,
-        addCommentLoading: true,
-        addCommentDone: false,
-        addCommentError: null,
-      }
-    case ADD_COMMENT_SUCCESS: {
-      const { content, postId, userId } = action.data
+  return produce(state, draft => {
+    switch (action.type) {
+      case ADD_POST_REQUEST:
+        draft.addPostLoading = true
+        draft.addPostDone = false
+        draft.addPostError = null
+        break
+      case ADD_POST_SUCCESS:
+        draft.mainPosts.unshift(dummyPost(action.data))
+        draft.addPostLoading = false
+        draft.addPostDone = true
+        break
+      case ADD_POST_FAILURE:
+        draft.addPostLoading = false
+        draft.addPostError = action.error
+        break
+      case REMOVE_POST_REQUEST:
+        draft.removePostLoading = true
+        draft.removePostDone = false
+        draft.removePostError = null
+        break
+      case REMOVE_POST_SUCCESS:
+        draft.mainPosts = draft.mainPosts.filter(v => v.id !== action.data)
+        draft.removePostLoading = false
+        draft.removePostDone = true
+        break
+      case REMOVE_POST_FAILURE:
+        draft.addCommentDone = false
+        draft.addCommentDoneremovePostError = action.error
+        break
+      case ADD_COMMENT_REQUEST:
+        draft.addCommentDoneaddCommentLoading = true
+        draft.addCommentDoneaddCommentDone = false
+        draft.addCommentDoneaddCommentError = null
+        break
+      case ADD_COMMENT_SUCCESS: {
+        const { content, postId, userId } = action.data
+        const post = draft.mainPosts.find(v => v.id === postId)
 
-      const postIndex = state.mainPosts.findIndex(v => v.id === postId)
-      const post = { ...state.mainPosts[postIndex] }
-      post.Comments = [dummyComment({ content, userId }), ...post.Comments]
-      const mainPosts = [...state.mainPosts]
-      mainPosts[postIndex] = post
-
-      return {
-        ...state,
-        mainPosts,
-        addCommentLoading: false,
-        addCommentDone: true,
+        post.Comments.unshift(dummyComment({ content, userId }))
+        draft.addCommentLoading = false
+        draft.addCommentDone = true
+        break
       }
+      case ADD_COMMENT_FAILURE:
+        draft.addCommentLoading = false
+        draft.addCommentError = action.error
+        break
+      default:
+        break
     }
-    case ADD_COMMENT_FAILURE:
-      return {
-        ...state,
-        addCommentLoading: false,
-        addCommentError: action.error,
-      }
-    default:
-      return state
-  }
+  })
 }
 
 export default reducer
