6c72744c9ac854dc0e648ce39f98767efa4c64b0
yusu Jang <dbtnss@naver.com>
2021-06-19 01:35:24 +0900

    update profile component

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Router from 'next/router'
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 import AppLayout from '../components/AppLayout'
@@ -8,18 +10,28 @@ import FollowList from '../components/FollowList'
 const Profile = () => {
   const { me } = useSelector(state => state.user)
 
-  return (
-    <>
-      <Head>
-        <title>내 프로필 | NodeBird</title>
-      </Head>
-      <AppLayout>
-        <NicknameEditForm />
-        <FollowList header="팔로잉" data={me.Followings} />
-        <FollowList header="팔로워" data={me.Followers} />
-      </AppLayout>
-    </>
-  )
+  useEffect(() => {
+    if (!me?.id) {
+      Router.push('/')
+    }
+  }, [me?.id])
+
+  if (!me) {
+    return null
+  } else {
+    return (
+      <>
+        <Head>
+          <title>내 프로필 | NodeBird</title>
+        </Head>
+        <AppLayout>
+          <NicknameEditForm />
+          <FollowList header="팔로잉" data={me.Followings} />
+          <FollowList header="팔로워" data={me.Followers} />
+        </AppLayout>
+      </>
+    )
+  }
 }
 
 export default Profile
