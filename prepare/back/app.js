add985ee37fc49091e42f04f70fdeb10e547ab17
peter <peter@gooroomee.com>
2021-06-20 18:56:36 +0900

    Use get, post

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,15 +1,31 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method)
-  res.write('<h1>Hello Node1</h1>')
-  res.write('Hello Node2')
-  res.write('Hello Node3')
-  res.write('Hello Node4')
-  res.write('Hello Node5')
-  res.write('Hello Node6')
-  res.end('Hello Node7')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello express')
 })
 
-server.listen(3065, () => {
+app.get('/api', (req, res) => {
+  res.send('hello api')
+})
+
+app.get('/api/post', (req, res) => {
+  res.json([
+    { id: 1, content: 'hello' },
+    { id: 1, content: 'hello' },
+    { id: 1, content: 'hello' }
+  ])
+})
+
+app.post('/api/post', (req, res) => {
+  res.json({ id: 1, content: 'hello'})
+})
+
+app.delete('/api/post', (req, res) => {
+  res.send({ id: 1 })
+})
+
+app.listen(3065, () => {
   console.log('서버 실행 중')
 })
