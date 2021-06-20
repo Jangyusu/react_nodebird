927039f142e943488cd277dbb3f47f032a69e86f
peter <peter@gooroomee.com>
2021-06-20 19:02:59 +0900

    Separate routes

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,16 +1,13 @@
 const express = require('express')
+const postRouter = require('./routes/post')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hello express')
-})
-
-app.get('/api', (req, res) => {
+router.get('/', (req, res) => {
   res.send('hello api')
 })
 
-app.get('/api/post', (req, res) => {
+router.get('/posts', (req, res) => {
   res.json([
     { id: 1, content: 'hello' },
     { id: 1, content: 'hello' },
@@ -18,14 +15,8 @@ app.get('/api/post', (req, res) => {
   ])
 })
 
-app.post('/api/post', (req, res) => {
-  res.json({ id: 1, content: 'hello'})
-})
+app.use('/post', postRouter)
 
-app.delete('/api/post', (req, res) => {
-  res.send({ id: 1 })
-})
-
-app.listen(3065, () => {
+router.listen(3065, () => {
   console.log('서버 실행 중')
 })
