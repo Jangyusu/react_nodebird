2cbad9ca5598d009ca8dadc6102865e4755a1265
yusu Jang <dbtnss@naver.com>
2021-06-21 22:11:39 +0900

    Update router -> app

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -3,11 +3,11 @@ const postRouter = require('./routes/post')
 
 const app = express()
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('hello api')
 })
 
-router.get('/posts', (req, res) => {
+app.get('/posts', (req, res) => {
   res.json([
     { id: 1, content: 'hello' },
     { id: 1, content: 'hello' },
@@ -17,6 +17,6 @@ router.get('/posts', (req, res) => {
 
 app.use('/post', postRouter)
 
-router.listen(3065, () => {
+app.listen(3065, () => {
   console.log('서버 실행 중')
 })
