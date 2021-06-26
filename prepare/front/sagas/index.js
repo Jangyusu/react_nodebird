fc17676c334e3a3e762433f48a654cc1d19a80a6
yusu <dbtnss@naver.com>
2021-06-27 03:28:34 +0900

    Update isNotLoggedIn error text
    Use withCredentials

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -5,6 +5,7 @@ import postSaga from './post'
 import userSaga from './user'
 
 axios.defaults.baseURL = 'http://localhost:3065'
+axios.defaults.withCredentials = true // saga에서 보내는 axios 요청, backend에 쿠기를 전달하기 위해 withCredentials = true
 
 export default function* rootSaga() {
   yield all([fork(postSaga), fork(userSaga)])
