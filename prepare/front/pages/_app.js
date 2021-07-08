07aef15f49dc09e10f0210dbe1037f6b7a4505ce
yusu Jang <dbtnss@naver.com>
2021-07-08 23:44:04 +0900

    Uninstall next-redux-saga

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types'
 import Head from 'next/head'
 import 'antd/dist/antd.css'
-import withReduxSaga from 'next-redux-saga'
 
 import wrapper from '../store/configureStore'
 
@@ -19,4 +18,4 @@ NodeBird.prototype = {
   Component: PropTypes.elementType.isRequired,
 }
 
-export default wrapper.withRedux(withReduxSaga(NodeBird))
+export default wrapper.withRedux(NodeBird)
